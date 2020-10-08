68ce4aa92fc7f0fb40a767c3e304e58ebbf16e5c
kevin_xie <xie.kevin.j@gmail.com>
2020-10-08 15:00:07 -0400

    before installing babel and stuff

--- k-react/src/Components/engine/index.js
+++ k-react/src/Components/engine/index.js
@@ -1,8 +1,70 @@
 import React from "react";
+import PropTypes from "prop-types";
+// import Matter from "matter-js";
+import Character from "../../views/home/character";
+
+import { AudioPlayer, Loop, Stage, KeyListener, World } from "react-game-kit";
 
 class Engine extends React.component {
+  static propTypes = {
+    onLeave: PropTypes.func,
+  };
+
+  componentDidMount() {
+    this.player = new AudioPlayer("/assets/Satellite.wav", () => {
+      this.stopMusic = this.player.play({
+        loop: true,
+        offset: 1,
+        volume: 0.35,
+      });
+    });
+
+    this.setState({
+      fade: false,
+    });
+
+    this.keyListener.subscribe([
+      this.keyListener.LEFT,
+      this.keyListener.RIGHT,
+      this.keyListener.UP,
+      this.keyListener.SPACE,
+      65,
+    ]);
+  }
+
+  componentWillUnmount() {
+    this.stopMusic();
+    this.keyListener.unsubscribe();
+  }
+
   render() {
-    return <div>Hello</div>;
+    return (
+      <Loop>
+        <Stage>
+          <World>
+            {/* <Level store={GameStore} /> */}
+            <Character
+            // onEnterBuilding={this.handleEnterBuilding}
+            // store={GameStore}
+            // keys={this.keyListener}
+            />
+          </World>
+        </Stage>
+      </Loop>
+    );
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fade: true,
+    };
+    this.keyListener = new KeyListener();
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    window.context = window.context || new AudioContext();
+
+    this.handleEnterBuilding = this.handleEnterBuilding.bind(this);
   }
 }
 
