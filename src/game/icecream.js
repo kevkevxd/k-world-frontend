2f1ec8cf78a41d3e57912bd7a7e8fe6fac9ee48b
kevin_xie <xie.kevin.j@gmail.com>
2020-10-11 22:12:49 -0400

    icecreams

--- src/game/icecream.js
+++ src/game/icecream.js
@@ -28,10 +28,20 @@ class Icecream extends React.Component {
   }
   render() {
     const x = this.props.store.characterPosition.x;
+    const icecreamColors = [
+      { primary: "#e7886e", shading: "#dc6646" },
+      { primary: "#e8f70f", shading: "#d8e60e" },
+      { primary: "#0ee6be", shading: "#08967c" },
+      { primary: "#65e08e", shading: "#42995f" },
+      { primary: "#3e4541", shading: "#2e3330" },
+    ];
+    const iceMap = icecreamColors.map(({ primary, shading }) => (
+      <IcecreamIcon style={{ width: 75 }} primary={primary} shading={shading} />
+    ));
     return (
       this.props.store.isIcecreamThere && (
         <div className="icecream" style={this.getWrapperStyles()}>
-          <IcecreamIcon style={{ width: 75 }} />
+          {iceMap}
           {/* <Body
             label="icecream"
             args={[x, 384, 120, 120]}
