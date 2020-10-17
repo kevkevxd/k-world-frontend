70e06f18d47d6e5dae0e55c37feb62e402b47448
kevinxie <kevin@DESKTOP-KAPJ3VG.localdomain>
2020-10-17 19:08:12 -0400

    flicker css, outro, starting iframe

---
+++ src/music/iframe.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const iframe = (props) => {
+    return (
+<div class="spotify-embed-block">
+  <iframe src="https://open.spotify.com/embed?uri=spotify:track:19RV3EkvLt39bWSZwsiGjy&theme=white"></iframe>
+</div>
+    )
+}
+export default iframe;
