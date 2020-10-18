e29e920acfae5ebf4726b699fcabce4225b06b90
kevinxie <kevin@DESKTOP-KAPJ3VG.localdomain>
2020-10-18 01:23:46 -0400

    intro text styling

--- src/intro/index.js
+++ src/intro/index.js
@@ -5,16 +5,20 @@ import "./intro.css"
 class Intro extends React.Component {
   render() {
     return (
-      <div className="intro">
-            <span id="G">G</span>
-            <span id="G">A</span>
-            <span id="G">L</span>
-            <span id="G">A</span>
-            <span id="G">X</span>
-            <span id="G">Y</span>
-            <span id="G">-</span>
-            <span id="I">K</span>
-            
+      <div className="intro-container">
+        <div className="intro">
+          <span id="G">G</span>
+          <span id="G">A</span>
+          <span id="G">L</span>
+          <span id="G">A</span>
+          <span id="G">X</span>
+          <span id="G">Y</span>
+          <span id="G">-</span>
+          <span id="I">K</span>
+      </div>
+      <div className="intro-text">
+        Press Enter to start
+      </div>
     </div>
     );
   }
