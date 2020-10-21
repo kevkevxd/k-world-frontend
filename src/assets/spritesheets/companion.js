5ee247783c7e34340aedf10b1f823dfac3042ca3
kevinxie <kevin@DESKTOP-KAPJ3VG.localdomain>
2020-10-21 12:28:16 -0400

    stuff

--- src/assets/spritesheets/companion.js
+++ src/assets/spritesheets/companion.js
@@ -2,13 +2,22 @@
 export const Companions = [
     {
         companion: "Poof",
-        companion_src: "assets/leattyspritesheet.png",
-       
+        companion_src: "assets/sprites/leattyspritesheet.png",
+
     },
     {
         companion: "Sus-Spaceman",
-        companion_src: "assets/sus.png",
-      
+        companion_src: "assets/sprites/sus.png",
+
     },
-    
-    ] 
+    {
+        companion: "Coco",
+        companion_src: "assets/sprites/coco.png",
+
+    },
+    {
+        companion: "Sophie",
+        companion_src: "assets/sprites/sophie.png",
+    },
+
+] 
