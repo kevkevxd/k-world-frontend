784019918af0125ab3b51ae0992fcc6d73284617
kevin xie <xie.kevin.j@gmail.com>
2021-01-11 04:36:48 -0500

    clientid

--- src/music/config.js
+++ src/music/config.js
@@ -4,15 +4,15 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "74b733b7e89f4c7bb98b3986710f9ad7";
 export const redirectUri = "https://k-world.herokuapp.com";
 export const scopes = [
-    "user-top-read",
-    "user-read-currently-playing",
-    "user-read-playback-state",
-    "user-read-private",
-    "user-read-email",
-    "user-modify-playback-state",
-    "playlist-read-collaborative",
-    "playlist-modify-private",
-    "playlist-read-private",
-    "playlist-modify-public",
-    "streaming",
+  "user-top-read",
+  "user-read-currently-playing",
+  "user-read-playback-state",
+  "user-read-private",
+  "user-read-email",
+  "user-modify-playback-state",
+  "playlist-read-collaborative",
+  "playlist-modify-private",
+  "playlist-read-private",
+  "playlist-modify-public",
+  "streaming",
 ];
